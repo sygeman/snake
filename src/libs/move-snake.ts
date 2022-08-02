3b18e022e841cfe8e8671ee00d4521da0df40276
Anton Savitskiy <sygeman@gmail.com>
2022-08-02 14:15:50 +0200

    Rewrite moveSnake

--- src/libs/move-snake.ts
+++ src/libs/move-snake.ts
@@ -1,37 +1,27 @@
 import { Direction } from "../types/direction";
 
+const directionTable: { [key: string]: [number, number] } = {
+  up: [-1, 0],
+  down: [1, 0],
+  left: [0, -1],
+  right: [0, 1],
+};
+
+const getNewHead = (
+  direction: Direction,
+  [ox, oy]: [number, number]
+): [number, number] => {
+  const [x, y] = directionTable[direction];
+  return [ox + x, oy + y];
+};
+
 export const moveSnake = (
   snake: [number, number][],
   direction: Direction,
   growth: boolean
 ) => {
-  const [head] = snake;
-  let newSnake: [number, number][] = [];
-
-  for (let i = 0; i < snake.length; i++) {
-    if (i === 0) {
-      switch (direction) {
-        case "up":
-          newSnake.push([head[0] - 1, head[1]]);
-          break;
-        case "down":
-          newSnake.push([head[0] + 1, head[1]]);
-          break;
-        case "left":
-          newSnake.push([head[0], head[1] - 1]);
-          break;
-        case "right":
-          newSnake.push([head[0], head[1] + 1]);
-          break;
-      }
-    } else {
-      newSnake.push(snake[i - 1]);
-    }
-  }
-
-  if (growth) {
-    newSnake.push(snake[snake.length - 1]);
-  }
-
-  return newSnake;
+  const size = growth ? snake.length + 1 : snake.length;
+  const [oldHead, ...tail] = snake;
+  const head = getNewHead(direction, oldHead);
+  return [head, oldHead, ...tail].slice(0, size);
 };
